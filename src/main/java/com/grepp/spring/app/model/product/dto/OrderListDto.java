63493ce92936e341f66b8c5b4dde12954a1f2265
namdabin <skaekqls789@naver.com>
2025-04-23 20:42:29 +0900

    product page 2차 커밋- db 연결

---
+++ src/main/java/com/grepp/spring/app/model/product/dto/OrderListDto.java
@@ -0,0 +1,30 @@
+package com.grepp.spring.app.model.product.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class OrderListDto {
+
+    private Long id;
+    private String email;
+    private String address;
+    private String addressNumber;
+    private LocalDateTime createdAt;
+    private List<ProductItemDTO> items;
+    private Boolean isMember;
+    private String userId;
+
+    @Data
+    public static class ProductItemDTO {
+
+        private Integer id; // product_id
+        private Double price;
+        private Integer amount;
+    }
+
+    public ProductItemDTO getItem() {
+        return items != null && !items.isEmpty() ? items.get(0) : null;
+    }
+}
--- src/main/java/com/grepp/spring/app/model/product/dto/ProductDto.java
+++ src/main/java/com/grepp/spring/app/model/product/dto/ProductDto.java
@@ -3,11 +3,15 @@ package com.grepp.spring.app.model.product.dto;
 import lombok.Data;
 
 @Data
-public class Product {
+public class ProductDto {
 
     private Integer id;
     private String name;
     private Integer price;
     private Integer amount;
     private String info;
+
+    private String originalFileName;
+    private String savePath; // 이미지 경로
+    private String renameFileName;
 }
